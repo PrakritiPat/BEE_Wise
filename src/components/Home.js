2e7b49155dbe2fc8d921374b563c6cc27b31e4ea
Prakriti Patnaik <prakritipatnaik123@gmail.com>
2025-02-15 13:43:36 -0800

    1:43pm

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,30 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
+import "./Home.css";
 
 
 const Home = () => {
   let navigate = useNavigate();
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Honey Tutoring</h1>
-      <p>This is the home page.</p>
-      <button onClick={() => navigate("/Login")}>Login</button>
-      <button onClick={() => navigate("/Login")}>SignUp</button>
+  <div className= "background-container">
+       <div style={{ textAlign: "center", marginTop: "50px" }}>
+
+<h1>Honey Tutoring</h1>
+<p>Empowering students, sustaining the future. We provide resources
+and support to help students thrive while promoting sustainable 
+practices that create a better tomorrow. Join us in making education 
+and the planet more sustainable, one step at a time.</p>
+<div className = "button-container">
+<button onClick={() => navigate("/Login")}>Login</button>
+<button onClick={() => navigate("/Login")}>SignUp</button>
+</div>
 
 
-    </div>
+
+</div>
+  </div>
+ 
+    
   );
 };
 
