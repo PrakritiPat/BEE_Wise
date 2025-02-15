a9ab6854f839464d7efbbbe1725c6ecf781ce89a
Prakriti Patnaik <prakritipatnaik123@gmail.com>
2025-02-15 15:12:21 -0800

    3:12

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from "react";
 import Home from "./components/Home";
 import Selection from "./components/Selection";
 import {BrowserRouter as Router, Routes, Route, useNavigate} from "react-router-dom"
+import Login from './login'; 
 
 
 function App() {
   return (
-    <Router>
+    <div className="App">
+      <Login />
+      <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Selection" element={<Selection />} />
         
       </Routes>
     </Router>
+    </div>
   );
 }
 export default App;
